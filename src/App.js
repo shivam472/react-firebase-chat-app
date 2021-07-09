93dacc74663ba7a9b22576d99ebe8a2cd01dcc15
shivam472 <shivamraj472@gmail.com>
2021-07-09 22:09:47 +0530

    added lazy loading in App.js and added a new UIfolder

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
+import React, { Suspense } from "react";
 import "./App.css";
 import { Route, Switch, Redirect } from "react-router-dom";
 import { auth } from "./firebase";
 import AuthPage from "./pages/AuthPage";
-import ChatPage from "./pages/ChatPage";
-import ProfilePage from "./pages/ProfilePage";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 import { useAuthState } from "react-firebase-hooks/auth";
+
+const ChatPage = React.lazy(() => import("./pages/ChatPage"));
+const ProfilePage = React.lazy(() => import("./pages/ProfilePage"));
 
 function App() {
   const [user] = useAuthState(auth);
 
   return (
     <div className="App">
-      <Switch>
-        <Route path="/auth">
-          {!user && <AuthPage />}
-          {user && <Redirect to="/profile" />}
-        </Route>
-
-        {user && (
-          <Route path="/chat">
-            <ChatPage />
+      <Suspense
+        fallback={
+          <div
+            style={{
+              height: "100vh",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/auth">
+            {!user && <AuthPage />}
+            {user && <Redirect to="/profile" />}
           </Route>
-        )}
 
-        {user && (
-          <Route path="/profile">
-            <ProfilePage />
+          {user && (
+            <Route path="/chat">
+              <ChatPage />
+            </Route>
+          )}
+
+          {user && (
+            <Route path="/profile">
+              <ProfilePage />
+            </Route>
+          )}
+
+          <Route path="*">
+            <Redirect to="/auth" />
           </Route>
-        )}
-
-        <Route path="*">
-          <Redirect to="/auth" />
-        </Route>
-      </Switch>
+        </Switch>
+      </Suspense>
     </div>
   );
 }
